52516527c54134b58238548de46d46f2065b7723
Ivan <icarpiodeveloper@gmail.com>
2024-10-16 21:15:57 +0200

    Working...

--- djangoDeploy/settings.py
+++ djangoDeploy/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-w^m)w*%%qnl)rmp&86u9v=v8*kq%+b+end$@kl2d8(em0)0o_7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['lamuela.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
