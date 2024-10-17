172e73ec169de4adba8a037d8443c50af374992f
icarpio <46372559+icarpio@users.noreply.github.com>
2024-10-17 11:13:48 +0200

    Update settings.py

--- djangoDeploy/settings.py
+++ djangoDeploy/settings.py
@@ -119,14 +119,13 @@ USE_TZ = True
 
 # settings.py
 STATIC_URL = '/static/'
-# This production code might break development mode, so we check whether we're in DEBUG mode
-if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+
+# En desarrollo, cuando DEBUG es True
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+    # En producción, cuando DEBUG es False
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
